ee55aac95bcff1d6c1b33ceb3ee09c9bcf2cb576
Sanish Kadariya <kadariyasanish01@gmail.com>
2023-03-02 17:53:55 -0500

    changed

---
+++ server/config/db.js
@@ -0,0 +1,5 @@
+module.exports = {
+  //local MongoDB deployment ->
+  //"URI": "mongodb://localhost/books229"
+  "URI":"mongodb+srv://Sanish:9RFZK9fn1ZEhMtIP@mongodbserver.kixa2y2.mongodb.net/Books?retryWrites=true&w=majority"
+};
